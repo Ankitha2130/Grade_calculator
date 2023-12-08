aa367a5f0d55e3d133a6d1a09d7adb23e88b0f55
Ankitha R <149484435+Ankitha2130@users.noreply.github.com>
2023-12-08 23:52:00 +0530

    Add files via upload

---
+++ JAVA Task2/Student_grade_calculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class Student_grade_calculator{
+    void grade_calculator(int[] array,int numberOfSubjects){
+        System.out.println("Total marks obtained is "+total_marks(array));
+        System.out.println("Average percentage obtained is "+average_percentage(array,numberOfSubjects));
+        grade_obtained(array);
+    }
+    int total_marks(int[] array){
+        int total=0;
+        for (int j=0;j<array.length;j++){
+            total += array[j];
+        }
+        return total;
+    }
+    double average_percentage(int[] array,int subjects){
+        double total=0.0;
+        for (int j=0;j<array.length;j++){
+            total += array[j];
+        }
+        return total/subjects;
+    }
+    void grade_obtained(int[] array){
+        int total=0;
+        for (int j=0;j<array.length;j++){
+            total += array[j];
+        }
+        if(total>=90){
+            System.out.println("Grade obtained is A+");
+        }
+        else if((total>=75)&&(total<90)){
+            System.out.println("Grade obtained is A");
+        }
+        else if((total>=65)&&(total<75)){
+            System.out.println("Grade obtained is B+");
+        }
+        else if((total>=55)&&(total<65)){
+            System.out.println("Grade obtained is B");
+        }
+        else if((total>=35)&&(total<55)){
+            System.out.println("Grade obtained is C+");
+        }
+        else{
+            System.out.println("Grade obtained is C");
+        }
+    }
+    public static void main(String args[]){
+        int numberOfSubjects;
+        System.out.println();
+        System.out.print("Enter number of subjects : ");
+        Scanner input = new Scanner(System.in);
+        numberOfSubjects=input.nextInt();
+        int[] array=new int[numberOfSubjects];
+        for (int i=0;i<numberOfSubjects;i++){
+            System.out.print("Enter subject "+(i+1)+" marks out of 100: ");
+            array[i]=input.nextInt();
+        }
+        System.out.println();
+        System.out.println("Results :");
+        System.out.println();
+        Student_grade_calculator obj1 = new Student_grade_calculator();
+        obj1.grade_calculator(array,numberOfSubjects);
+        System.out.println();
+    }
+}
